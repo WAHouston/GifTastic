047ade023c6231ce0b27f044e23cf1ca877f118c
Will H <wandrewhouston@gmail.com>
2018-09-27 17:27:26 -0400

    added ajax and gif display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,3 +16,27 @@ $("#create").on("click", function(event){
 
 makeButtons()
 
+$(document).on("click", "button" ,function() {
+    var newTopic = $(this).attr("data-newTopic")
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newTopic + "&api_key=" + apikey + "&limit=10"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(response) {
+        var results = response.data
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>")
+            var rating = results[i].rating
+            var p = $("<p>").text("Rating: " + rating)
+            var topicImage = $("<img>")
+            topicImage.attr("src", results[i].images.fixed_height.url)
+            gifDiv.prepend(p)
+            gifDiv.prepend(topicImage)
+            $("#gif-anchor").prepend(gifDiv)
+        }
+    })
+
+})
