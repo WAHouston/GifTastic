b620435d67bd21980024d241169317c94be24767
Will H <wandrewhouston@gmail.com>
2018-09-29 16:54:59 -0400

    added gif pause

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,11 +32,25 @@ $(document).on("click", "button" ,function() {
             var rating = results[i].rating
             var p = $("<p>").text("Rating: " + rating)
             var topicImage = $("<img>")
-            topicImage.attr("src", results[i].images.fixed_height.url)
+            topicImage.attr("src", results[i].images.fixed_height_still.url)
+            topicImage.attr("data-still", results[i].images.fixed_height_still.url)
+            topicImage.attr("data-animate", results[i].images.fixed_height.url)
+            topicImage.attr("data-state", "still")
             gifDiv.prepend(p)
             gifDiv.prepend(topicImage)
             $("#gif-anchor").prepend(gifDiv)
         }
     })
 
+})
+
+$(document).on("click", "img", function() {
+    var state = $(this).attr("data-state")
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"))
+        $(this).attr("data-state", "animate")
+    } else {
+        $(this).attr("src", $(this).attr("data-still"))
+        $(this).attr("data-state", "still")
+    } 
 })
